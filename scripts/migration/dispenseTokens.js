dd29b269bf403a31ff797d5b5c99702d5dbfb2ff
Evgeni <evgeni@leapdao.org>
2019-04-05 15:34:03 +0200

    balance ceck added to dispensing script

--- scripts/migration/dispenseTokens.js
+++ scripts/migration/dispenseTokens.js
@@ -2,18 +2,19 @@ const JSBI = require('jsbi');
 const ethers = require('ethers');
 const fs = require('fs');
 const csv = require('csv-parser');
-const { sendFunds } = './helpers';
+const { sendFunds, getBalance } = require ('./helpers');
 
 const nodeUrl = "http://node1.testnet.leapdao.org:8645";
 const rpc = new ethers.providers.JsonRpcProvider(nodeUrl);
 const dispenser = { 
-    address: '',
+    address: 'PUT DISPENSER ADDRESS HERE',
     priv: '!!!NEVER COMMIT WITH MAINNET PRIVATE KEY HERE!!!' //!!!NEVER COMMIT WITH MAINNET PRIVATE KEY HERE!!!
 }
 const snapshot = './snapshot.csv';
 
 async function run() {
     const balances = [];
+    let balance;
 
     fs.createReadStream(snapshot)  
     .pipe(csv())
@@ -24,6 +25,12 @@ async function run() {
         for(let i = 0; i < balances.length; i++) {
             console.log('Dispensng', balances[i].Balance, 'LEAP to', balances[i].Address);
             await sendFunds(dispenser, balances[i].Address, balances[i].Balance, rpc);
+            balance = await getBalance(balances[i].Address, rpc);
+            if (String(balance) === balances[i].Balance) {
+                console.log('   Done');
+            } else {
+                console.log('   Failed! Expected balance:', balances[i].Balance, 'actual: ', String(balance));
+            }
         }        
     });
 }
