cc8fb015f433220d98ab1eb9bd62b3629096c470
Marin Petrunić <marin.petrunic@gmail.com>
2019-04-08 15:47:51 +0200

    add query param parsing

--- utxo-metrics/src/index.ts
+++ utxo-metrics/src/index.ts
@@ -7,6 +7,29 @@ import {Web3Service} from "./services/web3Service";
 
 export default async function handler(event: APIGatewayProxyEvent, context: Context, callback: APIGatewayProxyCallback): Promise<void> {
     const service = new Web3Service('https://testnet-node.leapdao.org');
-    console.log(await service.getTransactionStatistic(new Date(2019, 3, 8, 8), new Date()));
-    return null;
+    const year = parseInt(event.queryStringParameters.year) || new Date().getFullYear();
+    const month = parseInt(event.queryStringParameters.month) || new Date().getMonth();
+    console.log({
+        from:  new Date(year, month-1, 1),
+        to: new Date(year, month, 1),
+    });
+    const stats = await service.getTransactionStatistic(
+        new Date(year, month-1, 1),
+        new Date(year, month-1, 31)
+    );
+    console.log({
+        from:  new Date(year, month-1, 1),
+        to: new Date(year, month-1, 31),
+        stats
+    });
+    callback(
+        null,
+        {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(stats)
+        }
+    )
 }
